73e32f3db8f751d20457cc30f68ec5eef4bbc4c0
dbmartins11 <gomartins5@hotmail.com>
2024-11-18 16:25:10 +0000

    create the roulette page

---
+++ src/views/RoulettePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
+
+function RoulettePage() {
+
+    return (
+        <>
+            <NavBar  />
+            <SideBar  />
+            
+        </>
+    );
+}
+
+const styles = {
+ 
+};
+
+export default RoulettePage;

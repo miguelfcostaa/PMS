00e4f1602eb338cfe00b40d56554bb4d2a6142d4
dbmartins11 <gomartins5@hotmail.com>
2024-11-19 23:09:09 +0000

    fix: fixed rerendering

--- src/views/RoulettePage.js
+++ src/views/RoulettePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useMemo } from 'react';
 import * as THREE from 'three';
 import NavBar from '../components/NavBar';
 import SideBar from '../components/SideBar';
@@ -18,26 +18,27 @@ function Init() {
     position = -1;
     useEffect(() => {
         scene = new THREE.Scene();
-        scene.background = (0xFF7F3E)
-        
+        scene.background = (0xFF7F3E);
+
         camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
         renderer = new THREE.WebGLRenderer();
+
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setClearColor(0xC7D5E5);
         canvas.current.appendChild(renderer.domElement);
 
-        const material = new THREE.LineBasicMaterial({color: 0xFAB12F});
-        
+        const material = new THREE.LineBasicMaterial({ color: 0xFAB12F });
+
         const points = [];
-        points.push( new THREE.Vector3( 0, 0.2, 0 ) );
-        points.push( new THREE.Vector3( 0, 0, 0 ) );
-        points.push( new THREE.Vector3( 0, 0.6, 0 ) );
-        
-        const geometry = new THREE.BufferGeometry().setFromPoints( points );
-        
-        const line = new THREE.Line( geometry, material );
+        points.push(new THREE.Vector3(0, 0.2, 0));
+        points.push(new THREE.Vector3(0, 0, 0));
+        points.push(new THREE.Vector3(0, 0.6, 0));
+
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+        const line = new THREE.Line(geometry, material);
         line.position.x = 0.2
-        scene.add( line );
+        scene.add(line);
 
         for (let c in colors) {
             let material;
@@ -65,38 +66,44 @@ function Init() {
         camera.position.z = 4;
 
         renderer.render(scene, camera);
+
+        return () => {
+            renderer.dispose();
+            //canvas.current.removeChild(renderer.domElement);
+          };
+
     }, []);
 }
 
 function RoulettePage() {
-    Init();    
+    Init()
     return (
         <>
             <NavBar />
             <SideBar />
             <div style={styles.canvas} ref={canvas}>
             </div>
-            
-            <div style={styles.container}>
-            {/* Buttons */}
-            <div style={styles.buttonGroup}>
-                <button style={{ ...styles.button, backgroundColor: "#9D0208" }}>
-                    Place Bet x2
-                </button>
-                <button style={{ ...styles.button, backgroundColor: "#009DFF" }}>
-                    Place Bet x10
-                </button>
-                <button style={{ ...styles.button, backgroundColor: "#000000"}}>
-                    Place Bet x2
-                </button>
-            </div>
 
-            <div style={styles.inputGroup}>
-                <label htmlFor="quantity" style={styles.label}>Quantity:</label>
-                <input type="number" id="quantity" style={styles.input} />
-                
+            <div style={styles.container}>
+                {/* Buttons */}
+                <div style={styles.buttonGroup}>
+                    <button style={{ ...styles.button, backgroundColor: "#9D0208" }}>
+                        Place Bet x2
+                    </button>
+                    <button style={{ ...styles.button, backgroundColor: "#009DFF" }}>
+                        Place Bet x10
+                    </button>
+                    <button style={{ ...styles.button, backgroundColor: "#000000" }}>
+                        Place Bet x2
+                    </button>
+                </div>
+
+                <div style={styles.inputGroup}>
+                    <label htmlFor="quantity" style={styles.label}>Quantity:</label>
+                    <input type="number" id="quantity" style={styles.input} />
+
+                </div>
             </div>
-        </div>
         </>
     );
 }
@@ -109,13 +116,13 @@ const styles = {
     container: {
         display: 'flex',
         flexDirection: 'column',
-        width:"100%",
+        width: "100%",
         alignItems: 'center',
-        marginLeft:"150px",
-        marginTop:"100px",
+        marginLeft: "150px",
+        marginTop: "100px",
     },
     buttonGroup: {
-        width:"100%",
+        width: "100%",
         display: 'flex',
         justifyContent: 'center',
         marginBottom: '10px',
@@ -128,12 +135,12 @@ const styles = {
         boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
         borderRadius: '20px',
         fontSize: '18px',
-        fontFamily:"Helvetica",
+        fontFamily: "Helvetica",
         color: "white",
         cursor: 'pointer',
     },
     inputGroup: {
-        width:"920px",
+        width: "920px",
         flexDirection: 'column',
         display: 'flex',
         justifyContent: 'center',
@@ -143,15 +150,15 @@ const styles = {
         margin: '5px',
         fontSize: '18px',
         color: "#A5A5A5",
-        fontFamily:"Helvetica",
+        fontFamily: "Helvetica",
     },
     input: {
         height: '30px',
-        width:"100%",
+        width: "100%",
         padding: '8px',
         fontSize: '14px',
         borderRadius: 20,
-        border:"none",
+        border: "none",
         boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     },
 };

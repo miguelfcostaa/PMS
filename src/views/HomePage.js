fb8a3bac8978a4df59142fe5a27f1f31438819df
SrCoelhoPT <becas541@gmail.com>
2024-11-20 19:22:24 +0000

    Pagina Home sem search e com campanhas pre definidas

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import NavBar from '../components/NavBar';
 import SideBar from '../components/SideBar';
+import CampaignBox from '../components/CampaignBox';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+    const [campaigns, setCampaigns] = useState([]);
+
+    useEffect(() => {
+        const fetchCampaigns = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/campaign/all-campaigns');
+                if (response.ok) {
+                    const data = await response.json();
+                    setCampaigns(data);  
+                } else {
+                    throw new Error('Erro ao buscar campanhas');
+                }
+            } catch (err) {
+                console.error('Erro na conexão', err);
+            }
+        };
+        fetchCampaigns();
+    }, []);
+
+    const navigate = useNavigate();
+
+    const handleCampaignClick = (id) => {
+        navigate(`/campaign/${id}`); // Redireciona para a página da campanha com o ID
+    };
 
     return (
-        <>   
+        <>               
             <div style={styles.mainContent}>
                 <div style={styles.container}>
                     <div style={styles.quadrado2}> </div>                    
@@ -15,18 +41,52 @@ function HomePage() {
 
                     <div style={styles.leftsection}>
                         <p style={styles.leftsection}>Your place to do <span style={{fontSize: '128px', fontWeight: '350'}}><b>GOOD!</b></span></p>
-                        <button style={styles.button1}>Start Creating Campaign </button>
+                        <a href='/create-campaign'> <button style={styles.button1}>Start Creating Campaign </button></a>
                         <p style={{fontSize: '32px', marginTop:'10%'}}>Start creating and <br/> organizing a campaign <br/><span style={{fontWeight: '350'}}><b>free of charge!</b></span></p>
                     </div>     
 
                     <div style={styles.rightsection}>
-                        <button style={styles.button}>Start Playing</button>
-                        <p>Play games and <span style={styles.highlight}>help</span> a campaign of <strong>your choice!</strong></p>
-                        <h2>Your chance to <span style={styles.highlight}>HELP!</span></h2>
-                    </div> 
-
-                                 
+                        <p style={{fontSize: '32px', marginTop:'10%'}}>Play games and <span style={{fontWeight: '350'}}><b>help</b></span> a <br/> campaign of <span style={{fontWeight: '350'}}><b>your choice!</b></span></p>
+                        <a href='/games'><button style={styles.button2}>Start Playing</button></a>
+                        <p style={styles.rightsection1}>Your chance to <br/><span style={{fontSize: '128px', fontWeight: '350'}}><b>HELP!</b></span></p>
+                    </div>   
+                <div style={styles.botcontainer}>
+                    <p style={{fontSize: '64px', textAlign: 'center',}}><b>POPULAR CAMPAIGNS</b></p>
+                    <div style={styles.campaignDisplay} >
+                            <div onClick={() => handleCampaignClick(campaigns._id)} style={{display:'flex', gap:'3%', width:'100%',   justifyContent: 'center'}}>
+                                <CampaignBox style={styles.campanha}
+                                    key="123"
+                                    title="123"
+                                    description="123"
+                                    goal="123"
+                                    timeToCompleteGoal="123"
+                                    currentAmount="123"
+                                    nameBankAccount="123"
+                                />      
+                                <CampaignBox style={styles.campanha}
+                                    key= '123'
+                                    title="123"
+                                    description="123"
+                                    goal="123"
+                                    timeToCompleteGoal="123"
+                                    currentAmount="123"
+                                    nameBankAccount="123"
+                                />   
+                                <CampaignBox style={styles.campanha}
+                                    key="123"
+                                    title="123"
+                                    description="123"
+                                    goal="123"
+                                    timeToCompleteGoal="123"
+                                    currentAmount="123"
+                                    nameBankAccount="123"
+                                />   
+                            </div>
+                    </div>
+                </div>     
                 </div>
+
+                
             </div>
             <NavBar  />
             <SideBar  />
@@ -36,15 +96,44 @@ function HomePage() {
 
 const styles = {
     
+    campanha:{
+        flex:'auto',
+        marginLeft: '5%',
+        height: '100%',
+        background: '#FFFFFF',
+        borderRadius: '10px'
+    },
+
+    campaignDisplay: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        marginRight: 30,
+        marginBottom: 30,
+        background: "#D9D9D9",
+        width:'100%'
+    },
+
     mainContent: {
         marginTop: 82,
         marginLeft: '20%',
-        marginRight: 0,
         paddingLeft: '0px',
         font: 'Inter',
-
     },
     
+    botcontainer: {
+        marginTop:"47%",
+        marginLeft:"-1.7%",
+        width: "80%",
+        height: "100%",
+        display: 'flex',
+        flexDirection: 'column',
+        marginBottom: 40,        
+        position:'absolute',
+        background: "#D9D9D9",
+    },
+
     container: {
         paddingLeft: '20px',
         width: "100%",
@@ -56,31 +145,32 @@ const styles = {
     },
 
     circulo1: {
-        height: "817px",
-        width: "997px",
+        height: "70%",
+        width: "30%",
         borderRadius: "100%",
         background: "#C7D5E5",
-        marginLeft:"-1%",
+        marginLeft:"15%",
+        marginTop:"-2%",
         position:'absolute',
         zindex:30
     },
 
     circulo2: {
-        height: "677px",
-        width: "1061px",
+        height: "80%",
+        width: "50%",
         borderRadius: "100%",
         background: "#425576",
-        marginLeft:"220px",
-        marginTop:"791px",
+        marginLeft:"17.3%",
+        marginTop:"29.48%",
         position:'absolute',
         zindex:30
     },
 
     quadrado1: {
-        marginLeft: -30,
+        marginLeft: "-1.5%",
         marginTop: 14,
-        height: "1011px",
-        width: "637px",
+        height: "99%",
+        width: "36.3%",
         borderRadius: "0%",
         background: "#C7D5E5",
         position:'absolute',
@@ -88,11 +178,11 @@ const styles = {
 
     quadrado2: {
         marginTop: 14,
-        height: "1011px",
-        width: "1009px",
+        height: "99%",
+        width: "45%", //1009
         borderRadius: "0%",
         background: "#425576",
-        marginLeft:"607px",
+        marginLeft:"33.3%",
         position:'absolute',
     },
 
@@ -107,23 +197,26 @@ const styles = {
     },
     
     rightsection:{
-       /**  display: 'flex',
-        flexdirection: 'column',
-        justifycontent: 'center',
-        alignitems: 'center',
-        backgroundcolor: '#3c4a63',
-        fontFamily: 'Inter',
+        textAlign: 'right',
         fontStyle: 'normal',
         fontSize: "64px",
-        position:"absolute"*/
-    },
-    
-    h2:{
-        margin: '0 0 20px',
+        fontWeight: "200",
+        position:"absolute",
+        color: '#C7D5E5',
+        marginLeft: '57%',
+        marginTop:'2.5%',
     },
 
-    h1:{
-        margin: '0 0 20px',
+    rightsection1:{
+        width:"150%",
+        textAlign: 'right',
+        fontStyle: 'normal',
+        fontSize: "64px",
+        fontWeight: "200",
+        position:"absolute",
+        color: '#C7D5E5',
+        marginTop:'50%',
+        marginLeft: '-50%'
     },
     
     highlight:{
@@ -146,15 +239,22 @@ const styles = {
         marginTop:"40%",
         boxShadow: '0.5vh 0.5vh 1vh rgba(0, 0, 0, 0.2)',
     },
-    
-    primary:{
-        backgroundcolor: '#526cb8', /* Button color */
-        color: 'white',
-    },
-    
-    secondary:{
-        backgroundcolor: '#d0d8ed', /* Light blue button */
-        color: '#3c4a63',
+
+    button2:{
+        padding: '10px 20px',
+        width: "398px",
+        height: "77px",
+        border: 'none',
+        background: '#C7D5E5',
+        boxshadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        borderradius: '5px',
+        cursor: 'pointer',
+        fontSize: '32px',
+        borderRadius: "20px",
+        color:"white",
+        marginLeft:"-100%",
+        marginTop:"40%",
+        boxShadow: '0.5vh 0.5vh 1vh rgba(0, 0, 0, 0.2)',
     },
 };
 
